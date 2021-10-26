451547604793cac4aa469c1a13ba8930a9057845
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-10-26 14:57:20 +0200

    end exercise email

--- email/js/script.js
+++ email/js/script.js
@@ -4,30 +4,54 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
 
 
-const userEmail = "porto_maso@grs.it"; //poi sostituire con n prompt
+// Creo la costante del bottone
+const btnCheck = document.getElementById('check');
 
-const databaseEmail = ["arianna.arco@gmail.com", "sofia.rossi@libero.it", 
-"amelia.nonloso@yahoo.com", 
-"ilenia1012@libero.it", 
-"isabella.gattini@gmail.com", 
-"matilda.guitar@gmail.com", 
-"fabio.gasolio@libero.it", 
-"luca.castronovo@gmail.com",
-"andrea.guitar@yahoo.it",
-"porto_maso@grs.it",
-"mobilinuovi@asrl.com"]
+// Creo la funzione al click del bottone
+btnCheck.addEventListener('click',function(){
 
-let verifiedEmail = false;
+  // Creo la costante email dell'utente
+  const userEmail = document.getElementById("email").value;
+  // console.log(userEmail);
 
+  // Array database Email 
+  const databaseEmail = ["arianna.arco@gmail.com", "sofia.rossi@libero.it", 
+  "amelia.nonloso@yahoo.com", 
+  "ilenia1012@libero.it", 
+  "isabella.gattini@gmail.com", 
+  "matilda.guitar@gmail.com", 
+  "fabio.gasolio@libero.it", 
+  "luca.castronovo@gmail.com",
+  "andrea.guitar@yahoo.it",
+  "porto_maso@grs.it",
+  "mobilinuovi@asrl.com"];
 
-for (let i = 0; i < databaseEmail.length; i++ ) {
-  console.log(databaseEmail[i]);
+  // Creo la variabile booleana di verifica dell'email
+  let verifiedEmail = false;
 
-  if (databaseEmail[i] === userEmail) {
-    verifiedEmail = true;
+  // Creo un ciclo per avere tutte l'email del database
+  for (let i = 0; i < databaseEmail.length; i++ ) {
+    // console.log(databaseEmail[i]);
+
+    // Se l'email inserita dall'utente è uguale a una dell'email presente nel database la variabile "verifiedEmail" è vera
+    if (databaseEmail[i] === userEmail) {
+      verifiedEmail = true;
+    }
   }
-}
 
-if (verifiedEmail === true) {
-  console.log ("puoi accedere");
-}
+  // console.log("email verificata",verifiedEmail);
+
+   // Creo due percorsi in base alla variabile "verifiedEmail" (se è vera o falsa)
+  if (verifiedEmail === true) {
+    const succes = document.getElementById("succes");
+    succes.className = "alert alert-success ls-display-block"
+    const form = document.getElementById("form");
+    form.className = "ls-display-none";
+  } else {
+    const wrong = document.getElementById("wrong");
+    wrong.className = "alert alert-danger ls-display-block"
+    const form = document.getElementById("form");
+    form.className = "ls-display-none";
+  }
+  
+});
