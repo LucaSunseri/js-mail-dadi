e14a304cb8cada2ec8be5eb168e1a0ab5dd47d0c
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-10-26 17:14:11 +0200

    add comment js

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,24 +2,30 @@
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
-const nameUser = document.getElementById('user').innerHTML = "Luca"; //inserire prompt
+// Inizializzo le costante nome giocatore
+const nameUser = document.getElementById('user').innerHTML = prompt("Come ti chiami?"); 
 
+// Inizializzo le costante bottone
 const startGame = document.getElementById("start");
 // console.log(startGame);
 
+// Funzione che da il via al gioco al click del bottone
 startGame.addEventListener("click",function(){ 
 
+  // Inizializzo le costanti dei dadi per il pc e l'utente
   const numberPc = Math.floor(Math.random() * 6) + 1;
     // console.log(numberPc);
   const dicePc = document.getElementById('dice-pc').innerHTML = numberPc;
 
   const numberUser = Math.floor(Math.random() * 6) + 1;
   const diceUser = document.getElementById('dice-user').innerHTML = numberUser;
+  
+  // Inizializzo le costanti per il risultato
+  const win = document.getElementById("win");
+  const same = document.getElementById("same");
+  const lose = document.getElementById("lose");
 
-  let win = document.getElementById("win");
-  let same = document.getElementById("same");
-  let lose = document.getElementById("lose");
-
+  // Aggiungo le condizione per il risultato finale
   if (numberPc < numberUser) {
     lose.classList.add("ls-hide");
     same.classList.add("ls-hide");
